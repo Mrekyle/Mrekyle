58742eed8464c127afc6a80fcd9ea25bd9df94a9
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2024-08-07 09:02:34 +0100

    Create base_env file

---
+++ base_env.py
@@ -0,0 +1,26 @@
+import os
+
+# BASE 
+
+os.environ['SECRET_KEY'] = ''
+os.environ['DEVELOPMENT_HOST'] = '127.0.0.0'
+os.environ['ALLOWED_HOST'] = ''
+
+# DATABASE
+
+os.environ['DATABASE_URL'] = ''
+
+# AWS
+
+# USE_AWS
+os.environ['AWS_ACCESS_KEY_ID'] = ''
+os.environ['AWS_SECRET_ACCESS_KEY'] = ''
+os.environ['AWS_S3_CUSTOM_DOMAIN'] = ''
+
+# EMAIL 
+
+os.environ['EMAIL_HOST_USER'] = ''
+os.environ['EMAIL_HOST_PASSWORD'] = ''
+
+# STRIPE
+
